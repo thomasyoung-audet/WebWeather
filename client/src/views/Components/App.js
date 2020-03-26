f438e35183632c55491728a1cbea06a05c7d550f
Thomas <thomasyoungaudet@gmail.com>
2020-03-26 13:33:01 -0400

    reset

--- client/src/views/Components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = ({ children }) => (
-  <main>
-    {children}
-  </main>
-);
-
-export default App;
--- client/src/views/Containers/index.js
+++
@@ -1,5 +0,0 @@
-import { default as Home } from './Home';
-import { default as CurrentWeather } from './CurrentWeather';
-//import { default as RadioButton } from './RadioButton';
-
-export { Home, CurrentWeather };
